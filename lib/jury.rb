8d8a99fe806b691b0f9924b3b373c2afaabfd565
Miguel Flores Silverio <floresmigu3l@gmail.com>
2016-02-29 04:46:48 -0800

    Passes jury tests

--- lib/jury.rb
+++ lib/jury.rb
@@ -1,2 +1,36 @@
 class Jury
+    attr_accessor :members
+    :votes
+    def initialize
+        @members = []
+        @votes = {}
+    end
+
+    def add_member(new_member)
+        @members.push(new_member)
+    end
+
+    def cast_votes(finalists)
+        finalists.each do |finalist|
+            @votes[finalist] = 0
+        end
+
+        members.each do |member|
+            finalist = finalists[rand(2)]
+            @votes[finalist] += 1
+            puts "#{member} #{finalist}"
+        end
+        @votes
+    end
+
+    def report_votes(votes)
+        votes.each do |key, value|
+            puts "#{key} #{value}"
+        end
+    end
+
+    def announce_winner(votes)
+        votes.max_by{|finalist,value| value}[0]
+    end
+
 end
